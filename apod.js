e6f0fee730b233dc892e4612d771bfb428dccd9f
unknown <iata.anderson15@gmail.com>
2022-01-30 15:44:35 -0300

    subindo apod

---
+++ apod.js
@@ -0,0 +1,14 @@
+function clickNasa() {
+    var data = document.getElementById("data").value 
+    $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=pHsnT7tQ99igunba5HszJ0X9ThO7eQmZMYUcA1T8date=${data}` ,
+    success: function( param){
+    $("#image").attr('src' ,param.url)
+    $('#title').text( param.title)
+    $('#description').text( param.explanation)
+    },
+    error: function(erro){
+        console.log(`${erro.result + erro.status} Deu ruim `);
+    },
+});
+};
+clickNasa()
